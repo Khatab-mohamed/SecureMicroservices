b9588e865481f43ceddb523ece9ef67db429a731
Khatab <khatab.mohammed@mdd.sa>
2024-05-13 02:45:47 -0900

    Get Token from IS4 client credentials, grant type

---
+++ Microservices/IdentityServer/Config.cs
@@ -0,0 +1,47 @@
+﻿global using IdentityServer4.Models;
+using IdentityServer4.Test;
+
+namespace IdentityServer;
+
+public class Config
+{
+
+    /// <summary>
+    /// Defines all the clients interacting with our resources.
+    /// </summary>
+    public static IEnumerable<Client> Clients => new Client[]
+    {
+        new() {
+            ClientId ="moviesClient",
+            AllowedGrantTypes = GrantTypes.ClientCredentials,
+            ClientSecrets =
+            {
+                new Secret("secret".Sha256()),
+
+            },
+            AllowedScopes={"moviesAPI"}
+        }
+    };
+
+    /// <summary>
+    /// Defines all the api scopes.
+    /// </summary>
+    public static IEnumerable<ApiScope> ApiScopes =>
+        new ApiScope[] {
+            new("moviesAPI","Movies APi")
+
+    };
+    /// <summary>
+    /// Defines all the data resources.
+    /// </summary>
+    public static IEnumerable<ApiResource> ApiResources => new ApiResource[] { };
+    /// <summary>
+    /// Defines all the data identity resources.
+    /// </summary>
+    public static IEnumerable<IdentityResource> IdentityResources => new IdentityResource[] { };
+    /// <summary>
+    /// Defines all the test users.
+    /// </summary>
+    public static List<TestUser> TestUsers => new List<TestUser> { };
+
+}
--- Microservices/IdentityServer/Program.cs
+++ Microservices/IdentityServer/Program.cs
@@ -1,16 +1,19 @@
+using IdentityServer;
 using IdentityServer4.Models;
 using IdentityServer4.Test;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddIdentityServer()
-    .AddInMemoryClients(new List<Client>()) // Ensure these have valid configurations
-    .AddInMemoryIdentityResources(new List<IdentityResource>())
-    .AddInMemoryApiResources(new List<ApiResource>())
-    .AddInMemoryApiScopes(new List<ApiScope>())
-    .AddTestUsers(new List<TestUser>())
+    .AddInMemoryClients(Config.Clients) // Ensure these have valid configurations
+    //.AddInMemoryIdentityResources(Config.IdentityResources)
+    //.AddInMemoryApiResources(Config.ApiResources)
+    .AddInMemoryApiScopes(Config.ApiScopes)
+    //.AddTestUsers(Config.TestUsers)
     .AddDeveloperSigningCredential();
 
 var app = builder.Build();
