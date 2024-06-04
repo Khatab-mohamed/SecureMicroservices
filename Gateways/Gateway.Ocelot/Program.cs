66b464f37684abf6cf1bcbefc47e0d12c53b8b60
Khatab <khatab.mohammed@mdd.sa>
2024-06-04 01:24:11 -0900

    Ocelot Api Gateway

---
+++ Gateways/Gateway.Ocelot/Program.cs
@@ -0,0 +1,21 @@
+using Gateway.Ocelot;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build()
+            .Run();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+        .ConfigureAppConfiguration((hostingContext, config) =>
+        {
+            config.AddJsonFile("ocelot.json");
+        })
+        .ConfigureWebHostDefaults(webBuilder =>
+        {
+            webBuilder.UseStartup<Startup>();
+        });
+}
---
+++ Gateways/Gateway.Ocelot/Startup.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+namespace Gateway.Ocelot;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddOcelot();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public async void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseRouting();
+        
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+        await app.UseOcelot();
+    }
+}
